f79707e3ad0ebf420f25c50da671331b365cb2dc
Danyal Muneer <abbasidani82@gmail.com>
2023-03-05 01:59:59 +0500

    adding progress chart

--- App.js
+++ App.js
@@ -3,12 +3,16 @@
 import AppRout from "./src/navigations/AppRoute";
 import { StatusBar, View } from "react-native";
 import Colors from "./assets/theme/Colors.js";
+import OMoneyMockProfile from "./src/screens/OMoneyMockProfile";
 function App() {
   return (
-    <>
-      <StatusBar backgroundColor={Colors.secondary} barStyle="light-content" />
-      <AppRout />
-    </>
+    // <>
+    //   <StatusBar backgroundColor={Colors.secondary} barStyle="light-content" />
+    //   <AppRout />
+    // </>
+
+
+    <OMoneyMockProfile />
   );
 }
 export default App;
